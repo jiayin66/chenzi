82e7399a97809d0996e91ecfa2e9604e99f77a7c
jiayin <501023696@qq.com>
2018-06-21 23:04:11 +0800

    支持httpclient和字符的截取

---
+++ chenzi/src/main/java/com/chen/chenzi/ChenziApplication.java
@@ -0,0 +1,12 @@
+package com.chen.chenzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChenziApplication.class, args);
+	}
+}
