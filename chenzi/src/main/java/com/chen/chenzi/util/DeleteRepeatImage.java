4e15927ff814798a4c6b9946ab53d0618b7809c2
jiayin <501023696@qq.com>
2018-06-21 23:51:18 +0800

    增加可以删除重复文件的代码

---
+++ chenzi/src/main/java/com/chen/chenzi/util/DeleteRepeatImage.java
@@ -0,0 +1,36 @@
+package com.chen.chenzi.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 可以删除文件夹下的重复文件，原理是通过MD5加密
+ * @author yjy
+ *
+ */
+public class DeleteRepeatImage {
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		File file=new File("D:/weixin/WeChat Files/jiayinweixin520/Video");
+		File[] fileArray=file.listFiles();
+		System.out.println("总文件数为:"+fileArray.length);
+		Set set=new HashSet();
+		int x=0;
+		for(int i=0;i<fileArray.length;i++){
+			//System.out.println(fileArray[i].getAbsolutePath());
+			//String str=TestFile.fileMD5(fileArray[i].getAbsolutePath());
+			String str=MD5Util.getMD5(fileArray[i].getAbsolutePath(), "file");
+			set.add(str);
+			x++;
+			if(set.size()<x){
+				//System.out.println("发现重复照片");
+				fileArray[i].delete();
+				x--;
+			}
+			
+		}
+		System.out.println((fileArray.length-set.size())+"个重复文件已经删除！");
+	}
+}
